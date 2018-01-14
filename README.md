1b5d27a88ab4be77194ef2b985284eef9c201ba1
Alex Mao <alexmao88@gmail.com>
2018-01-13 17:50:41 -0800

    Eveything done

---
+++ README.md
@@ -0,0 +1,55 @@
+# Pre-work - *TipCalculator*
+
+**TipCalculator** is a tip calculator application for iOS.
+
+Submitted by: **Alex**
+
+Time spent: **12** hours spent in total
+
+## User Stories
+
+The following **required** functionality is complete:
+
+* [x] User can enter a bill amount, choose a tip percentage, and see the tip and total values.
+
+The following **optional** features are implemented:
+* [ ] Settings page to change the default tip percentage.
+* [ ] UI animations
+* [ ] Remembering the bill amount across app restarts (if <10mins)
+* [x] Using locale-specific currency and currency thousands separators.
+* [x] Making sure the keyboard is always visible and the bill amount is always the first responder. This way the user doesn't have to tap anywhere to use this app. Just launch the app and start typing.
+
+The following **additional** features are implemented:
+
+- [x] When the app starts, only the bill field is visible. Then, as the user starts typing, the tip percentages, tip amount, and total amount show up.
+- [x] The background is a light-blue color.
+
+## Video Walkthrough
+
+Here's a walkthrough of implemented user stories:
+
+https://imgur.com/a/wt7g9
+
+(Imgur no longer has direct links to files)
+
+GIF created with [LiceCap](http://www.cockos.com/licecap/).
+
+## Notes
+
+Describe any challenges encountered while building the app.
+
+## License
+
+Copyright [2018] [name of copyright owner]
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -10,9 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billLabel: UITextField!
+    
+    @IBOutlet weak var tipTitle: UILabel!
+    
+    @IBOutlet weak var tipLabel: UILabel!
+    
+    @IBOutlet weak var totalTitle: UILabel!
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    @IBOutlet weak var percentControl: UISegmentedControl!
+    
+    @IBOutlet weak var separator: UIView!
+    
+    var percentage = 0.1;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.billLabel.becomeFirstResponder();
+        
+        self.tipTitle.isHidden = true;
+        self.tipLabel.isHidden = true;
+        self.totalTitle.isHidden = true;
+        self.totalLabel.isHidden = true;
+        self.percentControl.isHidden = true;
+        self.separator.isHidden = true;
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +46,56 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onTap(_ sender: AnyObject) {
+        
+        view.endEditing(true);
+    }
+    
+   
+    @IBAction func billLabelChanged(_ sender: AnyObject) {
+        
+        self.tipTitle.isHidden = false;
+        self.tipLabel.isHidden = false;
+        self.totalTitle.isHidden = false;
+        self.totalLabel.isHidden = false;
+        self.percentControl.isHidden = false;
+        self.separator.isHidden = false;
+    }
 
+    
+    
+    
+    @IBAction func calculateTip(_ sender: UISegmentedControl) {
+        
+        let bill = Double(billLabel.text!) ?? 0
+        
+        switch percentControl.selectedSegmentIndex {
+        case 0:
+            percentage = 0.1;
+        case 1:
+            percentage = 0.15;
+        case 2:
+            percentage = 0.2;
+        default:
+            break;
+        }
+        
+        let tip = bill * percentage;
+        let total = bill + tip;
+        
+        let currencyFormatter = NumberFormatter();
+        currencyFormatter.usesGroupingSeparator = true;
+        
+        currencyFormatter.numberStyle = NumberFormatter.Style.currency;
+        
+        currencyFormatter.locale = NSLocale.current;
+        
+        
+        tipLabel.text = currencyFormatter.string(from:NSNumber(value:tip));
+        
+        totalLabel.text = currencyFormatter.string(from:NSNumber(value:total));
+    }
+    
+    
 }
 
